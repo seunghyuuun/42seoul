e126c2dc6531989c2d2a04b644cb088b92339f37
seunghyuuun <seunghy2@student.42seoul.kr>
2023-05-31 13:15:30 +0900

    Add files via upload

---
+++ libft/ft_lstlast_bonus.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_lstlast_bonus.c                                 :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: seunghy2 <marvin@42.fr>                    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/03/20 12:41:58 by seunghy2          #+#    #+#             */
+/*   Updated: 2023/03/20 12:42:01 by seunghy2         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "libft.h"
+
+t_list	*ft_lstlast(t_list *lst)
+{
+	t_list	*result;
+
+	if (!lst)
+		return (0);
+	result = lst;
+	while (result->next)
+	{
+		result = result->next;
+	}
+	return (result);
+}
