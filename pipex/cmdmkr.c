76799835bc3e3086800112b3a0c0b34488945b8b
seunghyuuun <seunghy2@student.42seoul.kr>
2023-07-04 20:00:33 +0900

    Add files via upload

--- pipex/cmdmkr.c
+++ pipex/cmdmkr.c
@@ -6,11 +6,26 @@
 /*   By: seunghy2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/19 12:10:24 by seunghy2          #+#    #+#             */
-/*   Updated: 2023/06/26 13:07:30 by seunghy2         ###   ########.fr       */
+/*   Updated: 2023/07/04 19:54:59 by seunghy2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex.h"
+
+void	cmdempty(char *str, char ***result, int i)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] == ' ')
+		index++;
+	if (!(str[index]))
+	{
+		result[i] = 0;
+		threedfree(result);
+		manualerror("pipex: command not found\n");
+	}
+}
 
 char	***cmdmkr(int argc, char **argv)
 {
@@ -24,6 +39,7 @@ char	***cmdmkr(int argc, char **argv)
 	i = 0;
 	while (i < argc - 3)
 	{
+		cmdempty(argv[i + 2], result, i);
 		result[i] = ft_split(argv[i + 2], ' ');
 		if (!result[i])
 		{
--- pipex/cmdmkr_bonus.c
+++ pipex/cmdmkr_bonus.c
@@ -6,11 +6,26 @@
 /*   By: seunghy2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/26 16:20:15 by seunghy2          #+#    #+#             */
-/*   Updated: 2023/06/26 16:23:52 by seunghy2         ###   ########.fr       */
+/*   Updated: 2023/07/04 19:57:33 by seunghy2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "pipex_bonus.h"
+
+void	cmdempty(char *str, char ***result, int i)
+{
+	int	index;
+
+	index = 0;
+	while (str[index] == ' ')
+		index++;
+	if (!(str[index]))
+	{
+		result[i] = 0;
+		threedfree(result);
+		manualerror("pipex: command not found\n");
+	}
+}
 
 char	***bs_cmdmkr(int argc, char **argv, int skip)
 {
@@ -24,6 +39,7 @@ char	***bs_cmdmkr(int argc, char **argv, int skip)
 	i = 0;
 	while (i < argc - (skip + 1))
 	{
+		cmdempty(argv[i + skip], result, i);
 		result[i] = ft_split(argv[i + skip], ' ');
 		if (!result[i])
 		{
