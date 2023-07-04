97c26da425725ef3cb18f180388de76fb1b458bc
seunghyuuun <seunghy2@student.42seoul.kr>
2023-07-04 16:11:50 +0900

    Add files via upload

--- pushswap/libft/ft_strjoin.c
+++ pushswap/libft/ft_strjoin.c
@@ -6,7 +6,7 @@
 /*   By: seunghy2 <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/16 21:27:16 by seunghy2          #+#    #+#             */
-/*   Updated: 2023/06/26 16:50:44 by seunghy2         ###   ########.fr       */
+/*   Updated: 2023/07/04 16:10:23 by seunghy2         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,21 +19,23 @@ char	*ft_strjoin(char const *s1, char const *s2)
 	char	*result;
 	size_t	j;
 
-	i = -1;
+	i = 0;
 	if (!s1 || !s2)
 		return (0);
 	len = ft_strlen(s1) + ft_strlen(s2);
 	result = (char *)malloc(len + 1);
 	if (!result)
 		return (0);
-	while (s1[++i])
+	while (s1[i])
+	{
 		result[i] = s1[i];
+		i++;
+	}
 	j = 0;
 	while (s2[j])
 	{
-		result[i] = s2[j];
+		result[i] = s2[j++];
 		i++;
-		j++;
 	}
 	result[i] = 0;
 	return (result);
